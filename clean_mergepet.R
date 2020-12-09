c5145209f51dff733b59b81bd3b0f58a8fb71cba
WillForan <willforan@gmail.com>
2020-12-09 07:50:37 -0500

    🎨 clean up merge pet cols (547 instead of 7000)

---
+++ clean_mergepet.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages{library(dplry)}
+
+# 20201209WF - init
+#   keep only rows we need in giant wide dataframe 
+
+keep_col_regex <- 'lunaid|^age|^visitnum|^sex|vdate|behdatestr|dtbzdate|^fd|uppsp|rist|rt18|tat2|frogET|ysrasr'
+
+read.csv('/Volumes/Phillips/mMR_PETDA/scripts/merged_data.csv') %>%
+  select(matches(keep_col_regex)) %>%
+  write.csv('data/wide.csv')
+
